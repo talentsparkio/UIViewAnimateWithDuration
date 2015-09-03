bf63ab4ae1751dd1fed70a27e5b3cab1886d7e58
Nick Chen <nchen.dev@mac.com>
2015-09-02 23:10:00 -0700

    Add auto layout storyboard setup

---
+++ UIViewAnimateWithDuration/AutoLayoutExampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AutoLayoutExampleViewController.swift
+//  UIViewAnimateWithDuration
+//
+//  Created by Nick Chen on 9/2/15.
+//  Copyright © 2015 Nick Chen. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
