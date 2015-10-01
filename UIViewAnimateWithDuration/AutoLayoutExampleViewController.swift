197182ceee07f3ed11072354edb1ffa7a287bdb8
Nick Chen <nchen.dev@mac.com>
2015-09-30 21:06:10 -0700

    Add UIDesignable example

--- UIViewAnimateWithDuration/AutoLayoutExampleViewController.swift
+++ UIViewAnimateWithDuration/AutoLayoutExampleViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class AutoLayoutExampleViewController: UIViewController {
-
+    
+    @IBOutlet weak var rightViewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftRightGapConstraint: NSLayoutConstraint!
+    
+    var hasSlideYet = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateConstraints()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func slideToggle(sender: UIButton) {
+        hasSlideYet = !hasSlideYet
+        UIView.animateWithDuration(1.0, animations: {
+            self.updateConstraints()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func updateConstraints() {
+        if(hasSlideYet) {
+            // slide it off
+            leftRightGapConstraint.constant = 20.0
+            rightViewConstraint.priority = UILayoutPriorityDefaultHigh - 1
+        } else {
+            // slide it back
+            leftRightGapConstraint.constant = NSLayoutConstraint.standardConstantBetweenSiblings()
+            rightViewConstraint.priority = UILayoutPriorityDefaultHigh + 1
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extension NSLayoutConstraint {
+    class func standardConstantBetweenSiblings() -> CGFloat {
+        let view = UIView()
+        let constraintWithStandardConstantBetweenSiblings = NSLayoutConstraint.constraintsWithVisualFormat("[view]-[view]",
+            options: NSLayoutFormatOptions.DirectionLeadingToTrailing,
+            metrics: nil,
+            views: ["view" : view])
+        return constraintWithStandardConstantBetweenSiblings[0].constant ;    // 8.0
+    }
+    
+    class func standardConstantBetweenSuperview() -> CGFloat {
+        let view = UIView()
+        let superview = UIView()
+        superview.addSubview(view)
+        let constraintWithStandardConstantBetweenSuperview = NSLayoutConstraint.constraintsWithVisualFormat("|-[view]",
+            options: NSLayoutFormatOptions.DirectionLeadingToTrailing,
+            metrics: nil,
+            views: ["view" : view])
+        return constraintWithStandardConstantBetweenSuperview[0].constant ;    // 20.0
     }
 }
--- UIViewAnimateWithDuration/MyCustomView.swift
+++ UIViewAnimateWithDuration/MyCustomView.swift
@@ -8,11 +8,61 @@
 
 import UIKit
 
+@IBDesignable
 class MyCustomView: UIView {
 
     // This is here to force auto layout to work
     override func intrinsicContentSize() -> CGSize {
         return CGSizeMake(200, 200)
+    }
+    
+    @IBInspectable var fillColor: UIColor = UIColor.greenColor()
+    @IBInspectable var plusColor: UIColor = UIColor.whiteColor()
+
+    // See the following link for a full example
+    // http://www.raywenderlich.com/90690/modern-core-graphics-with-swift-part-1
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath(ovalInRect: rect)
+        fillColor.setFill()
+        path.fill()
+        
+        //set up the width and height variables
+        //for the horizontal stroke
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        //create the path
+        let plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = 5.0
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2 - plusWidth/2,
+            y:bounds.height/2))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2 + plusWidth/2,
+            y:bounds.height/2))
+        
+        //move to the start of the vertical stroke
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2,
+            y:bounds.height/2 - plusWidth/2))
+        
+        //add the end point to the vertical stroke
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2,
+            y:bounds.height/2 + plusWidth/2))
+
+        
+        //set the stroke color
+        plusColor.setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
     }
 
 
